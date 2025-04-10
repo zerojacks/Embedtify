bc6c5e5041e2fb5155605dbebfc84fead447fbbf
zerojacks <embedtalkdev@gmail.com>
2025-04-10 11:51:49 +0800

    fix:修复websockete

--- frontend/src/Provider/WebSocketManager.tsx
+++ frontend/src/Provider/WebSocketManager.tsx
@@ -1,6 +1,9 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect } from 'react';
 import { SocketProvider } from './webProvider';
 import { useTestPlanWebSocket } from '@/hooks/useTestPlanWebSocket';
+import { ExecTestedPlanData, ExectestPlan } from '@/types/testplan';
+import { getAllExecutedPlanAPI } from '@/api/PlanAPI';
+import { useTestingStore } from '@/stores/useTestingStore';
 
 interface WebSocketManagerProps {
     children: ReactNode;
@@ -11,6 +14,26 @@ const WebSocketListener: FC = () => {
     return null;
 };
 export const WebSocketManager: FC<WebSocketManagerProps> = ({ children, url }) => {
+    const { testPlans, addTestPlan } = useTestingStore();
+    useEffect(() => {
+        async function fetchTestedResult() {
+            try {
+                const result: ExecTestedPlanData = await getAllExecutedPlanAPI();
+                if (result?.data) {
+                    result.data.forEach((item: ExectestPlan) => {
+                        console.log("item", item);
+                        if (!testPlans[item.id]) {
+                            addTestPlan(item.id, item.data);
+                        }
+                    });
+                }
+            } catch (error) {
+                console.error("Error fetching test results:", error);
+            }
+        }
+        fetchTestedResult();
+    }, []);
+    
     return (
         <SocketProvider url={url}>
             <WebSocketListener />
